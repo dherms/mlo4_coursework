175a327261513f6c52f0cb28973c12012f82bfe1
David Herman-Ifueko <david.herman-ifueko@archemedx.com>
2022-10-08 12:55:16 +0700

    Week 4 simple Dockerfile and main

--- assignments/week-4/main.py
+++ assignments/week-4/main.py
@@ -5,6 +5,11 @@ import requests
 #We generate a new FastAPI app in the Prod environment
 #https://fastapi.tiangolo.com/
 
+app = FastAPI(title='Service')
 
 #Call your get function for a health Check
+@app.get("/", tags=["Health Check"])
+async def root():
+    #requests.get()
+    return {"message": "Ok"}
 #to receive both (face-bokeh and face-emotion)
