c269974156a6019673e027ca7d3d4915bf9f422c
David Herman-Ifueko <david.herman-ifueko@archemedx.com>
2022-10-08 13:23:18 +0700

    First attempt at docker-compose

--- assignments/week-4/main.py
+++ assignments/week-4/main.py
@@ -10,6 +10,7 @@ app = FastAPI(title='Service')
 #Call your get function for a health Check
 @app.get("/", tags=["Health Check"])
 async def root():
-    #requests.get()
-    return {"message": "Ok"}
+    bokeh_response = requests.get("http://face-bokeh:8000/")
+    #emotion_response = requests.get("http://face-emotion:8000/")
+    return bokeh_response.json
 #to receive both (face-bokeh and face-emotion)
