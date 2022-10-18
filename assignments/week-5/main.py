fa00559c4322d616ef665778feac09977505e4a3
David Herman-Ifueko <david.herman-ifueko@archemedx.com>
2022-10-17 18:00:32 -0700

    Uploaded my solution. It is different from Ali's
    My server complained that the variable names in the `face-emotion`
    config were incorrect so I changed them to conform. Also, the
    original instructions expected the main FastAPI server to be
    exposed on port `8000`, so I retained that and did not expose
    Triton on the host.

--- assignments/week-5/main.py
+++ assignments/week-5/main.py
@@ -9,3 +9,20 @@ import requests
 
 #Call your get function for a health Check
 #to receive both (face-bokeh and face-emotion)
+app = FastAPI(title="Triton Health Check")
+
+@app.get("/", tags=["Health Check"])
+async def root():
+  bokeh_response = requests.get("http://face-bokeh:8000/")
+  emotion_response = requests.get("http://face-emotion:8000/")
+  triton_response_code = requests.get("http://triton:8000/v2/health/ready").status_code
+  if triton_response_code == 200:
+      triton_response = "Ok"
+  else:
+      trition_response = "Error"
+  response = {
+      "face-bokeh": bokeh_response.json()["message"],
+      "face-emotion": emotion_response.json()["message"],
+      "triton": triton_response
+  }
+  return response
